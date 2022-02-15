21d3b74b0883205b0d71dcb9a547577676322acf
Carolina Silva <carolsilva.acfs@gmail.com>
2022-02-15 15:42:35 +0000

    Update settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,17 +33,15 @@
 ;; Startup
 (setq inhibit-startup-screen t)
 
-;; Backups
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-
 ;; MacOS
 (when (eq system-type 'darwin)  
   (setq ns-use-native-fullscreen t
-        mac-option-key-is-meta nil
-        mac-command-key-is-meta t
         mac-command-modifier 'meta
-        mac-option-modifier nil
-        mac-use-title-bar nil))
+        mac-option-modifier 'none
+        mac-right-option-modifier 'super))
+
+;; Backups
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 ;; Auto Generated
 (custom-set-variables
@@ -51,7 +49,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(goto-last-change evil nano-theme nano-modeline)))
+ '(package-selected-packages '(nano-theme nano-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
